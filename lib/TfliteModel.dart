ebe27c5280225f0850f2d5aa9072b14d7292816f
Cu Tiến <66855910+shinoasada1907@users.noreply.github.com>
2022-09-18 21:19:11 +0700

    Demo Công nghệ phần mềm

---
+++ lib/TfliteModel.dart
@@ -0,0 +1,127 @@
+// ignore_for_file: file_names
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+class TfliteModel extends StatefulWidget {
+  const TfliteModel({Key? key}) : super(key: key);
+
+  @override
+  _TfliteModelState createState() => _TfliteModelState();
+}
+
+class _TfliteModelState extends State<TfliteModel> {
+  late File _image;
+  late List _results;
+  bool imageSelect = false;
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    String res;
+    res = (await Tflite.loadModel(
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt"))!;
+  }
+
+  Future imageClassification(File image) async {
+    final List? recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _results = recognitions!;
+      _image = image;
+      imageSelect = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter demo"),
+        ),
+        body: ListView(
+          children: [
+            (imageSelect)
+                ? Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Image.file(_image),
+                  )
+                : Container(
+                    margin: const EdgeInsets.all(10),
+                    child: const Opacity(
+                      opacity: 0.8,
+                      child: Center(
+                        child: Text("No image selected"),
+                      ),
+                    ),
+                  ),
+            SingleChildScrollView(
+              child: Column(
+                children: (imageSelect)
+                    ? _results.map((result) {
+                        return Card(
+                          child: Container(
+                            margin: const EdgeInsets.all(10),
+                            child: Text(
+                              "${result['label']} - ${result['confidence'].toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  color: Colors.red, fontSize: 20),
+                            ),
+                          ),
+                        );
+                      }).toList()
+                    : [],
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: pickImageGallery,
+              tooltip: "Pick Image",
+              child: const Icon(Icons.image),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: pickImageCamera,
+              tooltip: "Pick Camera",
+              child: const Icon(Icons.camera_alt_outlined),
+            ),
+          ],
+        ));
+  }
+
+  Future pickImageGallery() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    File image = File(pickedFile!.path);
+    imageClassification(image);
+  }
+
+  Future pickImageCamera() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.camera,
+    );
+    File image = File(pickedFile!.path);
+    imageClassification(image);
+  }
+}
